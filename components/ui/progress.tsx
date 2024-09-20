a53dc815df2fa0e71718063760cfe8458ff4f570
Axel See Toh <seetohaxel@gmail.com>
2024-09-21 02:25:44 +0800

    Settings + Profile page

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -22,7 +22,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full bg-primary transition-transform duration-700 ease-out"
+      className="h-full w-full bg-primary rounded-full transition-transform duration-700 ease-out"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
